34c13aa8f064f47d78454cf6f07a917128ea4885
root <root@runs-PC.runs.es>
2021-08-21 10:33:56 +0200

    spawned merchants

--- settings.lua
+++ settings.lua
@@ -1,11 +1,11 @@
 local modpath = ...
 
-eraz.settings = {}
-
 local settings = Settings(modpath .. "/eraz.conf")
 
-eraz.settings.spawn = settings:get_bool("spawn") or true
-eraz.settings.spawn_interval = tonumber(settings:get("spawn_interval"))
-eraz.settings.spawn_chance = tonumber(settings:get("spawn_chance"))
-eraz.settings.spawn_announce = settings:get_bool("spawn_announce") or true
-eraz.settings.lifetime = tonumber(settings:get("lifetime"))
+eraz.settings = {
+	spawn = settings:get_bool("spawn") or true
+	spawn_interval = tonumber(settings:get("spawn_interval"))
+	spawn_chance = tonumber(settings:get("spawn_chance"))
+	spawn_announce = settings:get_bool("spawn_announce") or true
+	lifetime = tonumber(settings:get("lifetime"))
+}
